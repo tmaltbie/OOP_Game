25de4da92d93ed1a578d750cf71e32f914375aec
Trevor Maltbie <60497552+tmaltbie@users.noreply.github.com>
2020-07-17 21:52:51 -0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -3,15 +3,13 @@
  * app.js */
 
 let game
-const keyboard = document.getElementById('qwerty');
-const keys = document.querySelectorAll('.key')
 
 document.getElementById('btn__reset').addEventListener('click', () => {
     game = new Game
     game.startGame()
 })
 
-keyboard.addEventListener('click', e => {
+document.getElementById('qwerty').addEventListener('click', e => {
     if (e.target.tagName === 'BUTTON') {
         game.handleInteraction(e.target)
     }
@@ -19,6 +17,7 @@ keyboard.addEventListener('click', e => {
 
 document.addEventListener('keydown', e => {
     const overlay = document.getElementById('overlay');
+    const keys = document.querySelectorAll('.key')
     if (overlay.style.display === 'none') {
         for (let i = 0; i < keys.length; i++) {
             if (e.key === keys[i].textContent && keys[i].disabled != true) {
